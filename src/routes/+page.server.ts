005d4c6a6a16d6081474bba549fe4a51c19ec1d7
slythom <contact@sylvainthomas.fr>
2025-09-18 23:17:03 +0200

    Added: Search action

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,29 @@
 import type { Actions } from './$types';
+// TODO: typage / validation
 
 export const actions = {
-	get: async ({request}) => {
-		// TODO get formdata to build API endpoint 
-	},
-	search: async ({request}) => {
-		// TODO fetch API
-	},
-    save: async ({request}) => {
-		// TODO if the result is OK, the user click a button to save the card data (image, quantity...) into the SQLite DB
-	}
+    search: async ({ request, fetch }) => {
+        // TODO get formdata to build API endpoint 
+        const data = await request.formData();
+        const quantity = data.get('quantity');
+        const setcode = data.get('setcode');
+        const collectornumber = data.get('collectornumber');
+        const name = data.get('name');
+        const image_uri = data.get('image_uri');
+
+        const api_url = `https://api.scryfall.com/cards/${setcode}/${collectornumber}`
+        const res = await fetch(api_url)
+        console.log("données récupérées de l'API :", res)
+
+        if (!res.ok) {
+            return { success: false, status: res.status };
+        }
+
+        const card = await res.json();
+        return { success: true, card, quantity, name, image_uri };
+    },
+
+    save: async ({ request }) => {
+        // TODO if the result is OK, the user click a button to save the card data (image, quantity...) into the SQLite DB
+    }
 } satisfies Actions;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
+    import { Button } from "$lib/components/ui/button/index.js";
 </script>
 
-<form method="POST" action="?/get">
+<form method="POST" action="?/search">
 	<div class="mx-auto flex w-full max-w-sm flex-col gap-3 p-10">
 		<Label for="quantity">quantity</Label>
 		<Input type="number" name="quantity" placeholder="quantity" />
@@ -11,9 +12,13 @@
 		<Input type="text" name="setcode" placeholder="setcode" />
 		<Label for="collectornumber">collectornumber</Label>
 		<Input type="text" name="collectornumber" placeholder="collectornumber" />
-		<Label for="name">name</Label>
+		<!-- <Label for="name">name</Label>
 		<Input type="text" name="name" placeholder="name" />
 		<Label for="image_uri">image_uri</Label>
-		<Input type="text" name="image_uri" placeholder="image_uri" />
+		<Input type="text" name="image_uri" placeholder="image_uri" /> -->
+        <Button type="submit">Go!</Button>
 	</div>
 </form>
+<div>
+    <h1>Liste des cartes récupérées depuis l'API</h1>
+</div>
